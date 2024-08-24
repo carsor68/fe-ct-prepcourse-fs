ebbca88931dcfb517c90d74cd4e085d6e834c804
carsor68 <sorayahernandez1968@gmail.com>
2024-08-24 17:34:47 -0500

    tareamodulo6hastaT5

--- C6 Bucles/ejercicios/05.js
+++ C6 Bucles/ejercicios/05.js
@@ -2,6 +2,9 @@ function esVerdadero(valor) {
   // Si "valor" es verdadero, retorna "Soy verdadero".
   // Caso contrario, retorna "Soy falso".
   // Tu código:
+
+ return valor === true ? "Soy verdadero" : "Soy falso"
 }
+
 
 module.exports = esVerdadero;
--- C6 Bucles/ejercicios/06.js
+++ C6 Bucles/ejercicios/06.js
@@ -2,6 +2,18 @@ function tieneTresDigitos(num) {
   // Si el número recibido tiene tres dígitos, retorna true.
   // Caso contrario, retorna false.
   // Tu código:
+
+ //if(num > 99 && num < 1000) return true;
+ //else return false;
+
+
+// otra forma de hacerlo, convirtiendo el numero en string
+
+//if(num.toString().length === 3) return true;
+// else  return false;
+
 }
+
+
 
 module.exports = tieneTresDigitos;
